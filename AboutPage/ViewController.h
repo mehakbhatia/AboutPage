511b800afe13647c437d8eb6dfd22fe060ab5d0c
mehakbhatia <mehakbhatia22@gmail.com>
2012-05-29 18:02:43 +0530

    Sample code with UI elements and AboutPage

---
+++ AboutPage/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  AboutPage
+//
+//  Created by Mehak on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+IBOutlet UINavigationBar *navigationBar;
+IBOutlet UINavigationItem *about;
+}
+@property (retain ,nonatomic)IBOutlet UINavigationBar *navigationBar;
+@property(retain,nonatomic)IBOutlet UINavigationItem *about;
+
+@end
---
+++ AboutPage/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  AboutPage
+//
+//  Created by Mehak on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize navigationBar;
+@synthesize about;
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
